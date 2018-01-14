45e1b8fe460c0a9330ae3212ad843d455ed8b754
Sean Davis <seandavi@gmail.com>
2018-01-14 11:29:43 -0500

    convert bioc-devel to module

---
+++ modules/bioc-devel-ami/main.tf
@@ -0,0 +1,57 @@
+provider "aws" {
+  region                  = "us-east-1"
+  shared_credentials_file = "/Users/sdavis2/.aws/credentials"
+  profile                 = "ec2"
+}
+
+resource "aws_security_group" "rstudio" {
+  name = "rstudio"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # all output
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+}
+
+
+resource "aws_instance" "bioc_devel" {
+  ami = "ami-e3883599"
+  count = "${var.count}"
+  instance_type = "${var.instance_type}"
+  vpc_security_group_ids = ["${aws_security_group.rstudio.id}"]
+  user_data = "${file("${path.module}/userdata.sh")}"
+  tags {
+    Name = "${var.instance_name}-${count-index}"
+    Use  = "daily_work"
+    Category = "science"
+  }
+
+  # The name of our SSH keypair you've created and downloaded
+  # from the AWS console.
+  #
+  # https://console.aws.amazon.com/ec2/v2/home?region=us-west-2#KeyPairs:
+  #
+  key_name = "${var.key_name}"
+}
+
+output "public_ips" {
+  value = ["${aws_instance.bioc_devel.*.public_ip}"]
+}
--- modules/bioc-devel-ami/variables.tf
+++ modules/bioc-devel-ami/variables.tf
@@ -3,6 +3,11 @@ variable "instance_type" {
   default = "m5.xlarge"
 }
 
+variable "instance_name" {
+  description = "The instance base name"
+  default = "bioc-devel"
+}
+
 variable "key_name" {
   description = "This is the default port on which jupyter is running"
   default = "CSHLData.2015"
