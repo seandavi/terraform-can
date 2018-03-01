fe7e6df2a9d98f3da5e4a71126002cb79c380949
Sean Davis <seandavi@gmail.com>
2018-02-28 22:26:07 -0500

    add shiny server and miniconda to instance

--- modules/bioc-devel-ami/userdata.sh
+++ modules/bioc-devel-ami/userdata.sh
@@ -9,7 +9,11 @@ sudo su - \
 -c "R -e \"install.packages('shiny', repos='https://cran.rstudio.com/')\""
 sudo su - \
 -c "R -e \"install.packages('rmarkdown', repos='https://cran.rstudio.com/')\""
-sudo apt-get install gdebi-core
+apt-get install gdebi-core
 wget https://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.5.6.875-amd64.deb
-sudo dpkg -i shiny-server-1.5.6.875-amd64.deb
-
+dpkg -i shiny-server-1.5.6.875-amd64.deb
+apt-get install git
+wget http://repo.continuum.io/miniconda/Miniconda3-3.7.0-Linux-x86_64.sh -O /tmp/miniconda.sh
+bash /tmp/miniconda.sh -b -p /home/ubuntu/miniconda
+echo 'export PATH="/home/ubuntu/miniconda/bin:$PATH"' >> /home/ubuntu/.bashrc
+chown -R ubuntu:ubuntu /home/ubuntu/miniconda
